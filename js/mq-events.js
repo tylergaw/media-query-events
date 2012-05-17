9317a75f7c27b7e04b3c42b15a605c372f3f1f91
Tyler Gaw <me@tylergaw.com>
2012-05-17 14:49:13 -0400

    removing mql and some other kruft after feedback from the arc90 braintrust

--- js/mq-events.js
+++ js/mq-events.js
@@ -1,35 +1,24 @@
 (function () {
     var mqEvents = function (mediaChangeHandler) {
-        var sheets = document.styleSheets,
-            numSheets = sheets.length,
-            mqls = {},
-
-            // A default handler
-            mediaChange = function (mql) {
-                console.log(mql);
-            }
+        var sheets = document.styleSheets;
 
         // Use the provided handler if present
-        if (mediaChangeHandler) {
-            mediaChange = mediaChangeHandler;
+        if (!mediaChangeHandler) {
+            console.error('mqEvents needs a mediaChangeHandler to do its job.');
+            return false;
         }
 
-        for (var i = 0; i < numSheets; i += 1) {
-            var rules = sheets[i].cssRules,
-                numRules = rules.length;
+        for (var i = 0; i < sheets.length; i += 1) {
+            var rules = sheets[i].cssRules;
 
-            for (var j = 0; j < numRules; j += 1) {
-
+            for (var j = 0; j < rules.length; j += 1) {
                 // This Stackoverflow answer helped me figure out how
                 // to check the type of object each rule was
                 // http://stackoverflow.com/a/332429/368634
                 if (rules[j].constructor === CSSMediaRule) {
-                    // Use the i in the name or else multiple
-                    // stylesheets may cause members to be
-                    // overwritten.
-                    mqls['mql' + i + '-' + j] = window.matchMedia(rules[j].media.mediaText);
-                    mqls['mql' + i + '-' + j].addListener(mediaChange);
-                    mediaChange(mqls['mql' + i + '-' + j]);
+                    var mql = window.matchMedia(rules[j].media.mediaText);
+                    mql.addListener(mediaChangeHandler);
+                    mediaChangeHandler(mql);
                 }
             }
         }
